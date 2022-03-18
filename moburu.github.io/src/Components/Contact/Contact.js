90248c39bf568a3e47d2db8ec9e8caf272fa6746
Moburu <stevenschiffner3@gmail.com>
2022-03-17 23:18:06 -0400

    Changed contact section.

--- moburu.github.io/src/Components/Contact/Contact.js
+++ moburu.github.io/src/Components/Contact/Contact.js
@@ -1,51 +1,49 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './Contact.css';
+import email from './images/email.png';
+import linkedin from './images/linkedin.png';
+import github from './images/github.jpg';
 
 const Contact = () => {
+    const [status, setStatus] = useState("Submit");
     const handleSubmit = async (e) => {
-        e.preventDefault();
-        const { name, email, message } = e.target.elements;
-        let details = {
-            name: name.value,
-            email: email.value,
-            message: message.value,
-        };
-        let response = await fetch('http://localhost:5000/contact', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json;charset=utf-8'
-            },
-            body: JSON.stringify(details)
-        });
-        let result = await response.json();
-        alert(result.status);
-    }
+      e.preventDefault();
+      setStatus("Sending...");
+      const { name, email, message } = e.target.elements;
+      let details = {
+        name: name.value,
+        email: email.value,
+        message: message.value,
+      };
+      let response = await fetch("http://localhost:5000/contact", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json;charset=utf-8",
+        },
+        body: JSON.stringify(details),
+      });
+      setStatus("Submit");
+      let result = await response.json();
+      alert(result.status);
+};
 
     return (
         <div className='Contact'>
             <a className='anchor' id='contact'></a>
             <h1>Contact Me</h1>
-            <div className="inner-contact">
-                <div className='contact-map'>
-                    <iframe
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3020.4519800891903!2d-74.39087388426873!3d40.79606064040504!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c3a89613428bcf%3A0x950d2d9c1f811e1!2s250%20Ridgedale%20Ave%2C%20Florham%20Park%2C%20NJ%2007932!5e0!3m2!1sen!2sus!4v1647215198814!5m2!1sen!2sus"
-                        width="90%"
-                        height="90%"
-                        style={{border:0}}
-                        allowfullscreen=""
-                        loading="lazy"
-                        title="mymap"
-                        className="mymap">
-                    </iframe>
-                </div>
-                <div className='contact-form'>
-                    <form onSubmit={handleSubmit}>
-                        <input type='text' placeholder='Name' className='form-txt' /> <br />
-                        <input type='text' placeholder='Email' className='form-txt' /> <br />
-                        <textarea placeholder='Message' className='form-txtarea' /> <br />
-                        <input type='submit' name='Submit' className='form-submit' />
-                    </form>
-                </div>
+            <div className='contact-links'>
+                <a href="mailto:stevenschiffner3@gmail.com" className='icon-holder'>
+                    <img id="email" className='icon' src={email} alt="An email icon." />
+                    <span className='icon-title'>Email</span>
+                </a>
+                    <a href="https://www.linkedin.com/in/steven-schiffner-b972b5163/" className='icon-holder'>
+                    <img id="linkedin" className='icon' src={linkedin} alt="The LinkedIn logo." />
+                    <span className='icon-title'>LinkedIn</span>
+                </a>
+                <a href="https://github.com/Moburu" className='icon-holder'>
+                    <img id="github" className='icon' src={github} alt="The GitHub logo." />
+                    <span className='icon-title'>GitHub</span>
+                </a>
             </div>
         </div>
     );
@@ -53,3 +51,27 @@ const Contact = () => {
 
 
 export default Contact;
+
+
+{/* <div className="inner-contact">
+<div className='contact-map'>
+    <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3020.4519800891903!2d-74.39087388426873!3d40.79606064040504!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c3a89613428bcf%3A0x950d2d9c1f811e1!2s250%20Ridgedale%20Ave%2C%20Florham%20Park%2C%20NJ%2007932!5e0!3m2!1sen!2sus!4v1647215198814!5m2!1sen!2sus"
+        width="90%"
+        height="90%"
+        style={{border:0}}
+        allowFullScreen=""
+        loading="lazy"
+        title="mymap"
+        className="mymap">
+    </iframe>
+</div>
+<div className='contact-form'>
+    <form onSubmit={handleSubmit}>
+        <input type='text' placeholder='Name' className='form-txt' id='name' /> <br />
+        <input type='text' placeholder='Email' className='form-txt' id='email' /> <br />
+        <textarea placeholder='Message' className='form-txtarea' id='message' /> <br />
+        <button type='submit' className='form-submit'>{status}</button>
+    </form>
+</div>
+</div> */}
--- moburu.github.io/src/Components/Navbar/Navbar.js
+++ moburu.github.io/src/Components/Navbar/Navbar.js
@@ -1,10 +1,4 @@
 import React, { Component } from 'react';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-  } from "react-router-dom";
 import './Navbar.css';
 
 class Navbar extends Component {
@@ -21,19 +15,19 @@ class Navbar extends Component {
         return (
             <nav className="Navbar">
                 <h2 className="name">Steven Schiffner</h2>
-                <a href="/#" class="toggle-button" onClick={this.handleClick1}>
-                    <span class='bar'></span>
-                    <span class='bar'></span>
-                    <span class='bar'></span>
+                <a href="/#" className="toggle-button" onClick={this.handleClick1}>
+                    <span className='bar'></span>
+                    <span className='bar'></span>
+                    <span className='bar'></span>
                 </a>
                 <ul className='links'>
                     <li className='link'><a href="#about">About</a></li>
                     <li className='link'><a href="#projects">Projects</a></li>
                     <li className='link'><a href="#skills">Skills</a></li>
                     <li className='link'><a href='#contact'>Contact</a></li>
-                    <li className='resume'><a href="StevenSchiffnerResume.pdf" download>Resume</a></li>
-                    <li className='link'><a href="https://www.linkedin.com/in/steven-schiffner-b972b5163/" target="_blank" rel="noreferrer">LinkedIn</a></li>
-                    <li id='last-link' className='link'><a href="https://github.com/Moburu" target="_blank" rel="noreferrer">GitHub</a></li>
+                    <li id='last-link' className='resume'><a href="StevenSchiffnerResume.pdf" download>Resume</a></li>
+                    {/* <li className='link'><a href="https://www.linkedin.com/in/steven-schiffner-b972b5163/" target="_blank" rel="noreferrer">LinkedIn</a></li>
+                    <li className='link'><a href="https://github.com/Moburu" target="_blank" rel="noreferrer">GitHub</a></li> */}
                 </ul>
             </nav>
         );
